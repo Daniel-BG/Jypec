40d93bc98616fc200b1b7000a632d98766e1b3a6
Daniel-BG <danielbasgar@gmail.com>
2017-09-21 11:55:01 +0200

    Removed some dirty code. If needed again it is better to rewrite it

--- src/com/jypec/util/io/HyperspectralImageDataReader.java
+++ src/com/jypec/util/io/HyperspectralImageDataReader.java
@@ -1,15 +1,12 @@
 package com.jypec.util.io;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import com.jypec.img.HyperspectralImageData;
-import com.jypec.img.ImageDataType;
 import com.jypec.img.ImageHeaderData;
 import com.jypec.util.io.headerio.enums.BandOrdering;
 import com.jypec.util.io.headerio.enums.ByteOrdering;
@@ -60,60 +57,6 @@ public class HyperspectralImageDataReader {
 	 */
 	public static void readImageData(String fileName, HyperspectralImageData image, BandOrdering imageOrdering, ByteOrdering byteOrdering) {
 		readImageData(fileName, 0, image, imageOrdering, byteOrdering);
-	}
-	
-
-	
-
-	
-	
-	/**
-	 * @param file
-	 * @param bands bands in file
-	 * @param lines lines in file
-	 * @param samples samples in file
-	 * @param reqB bands to read
-	 * @param reqL lines to read
-	 * @param reqS samples to read
-	 * @param format format of the samples 
-	 * @param isLittleEndian if the format is lil endian
-	 * @return the image
-	 * @throws FileNotFoundException
-	 */
-	public static final HyperspectralImageData readSkippingBIP(String file, int bands, int lines, int samples, int reqB, int reqL, int reqS, ImageDataType format, boolean isLittleEndian) throws FileNotFoundException {
-		HyperspectralImageData hi = new HyperspectralImageData(null, format, reqB, reqL, reqS);
-		InputStream is = new FileInputStream(new File(file));	//read a file
-		is = new BufferedInputStream(is);						//buffer it
-		if (isLittleEndian) {
-			is = new EndiannessChangerReader(is, format.getByteDepth());
-		}
-		
-		IntegerReader bdr = new IntegerReader(is, format.getBitDepth());
-		
-		
-		for (int j = 0; j < lines; j++) {
-			for (int k = 0; k < samples; k++) {
-				for (int i = 0; i < bands; i++) {
-					try {
-						int data = bdr.read();
-						if (i < reqB && j < reqL && k < reqS) {
-							hi.setDataAt(data, i, j, k);
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-						System.exit(0);
-					}
-				}
-			}
-		}
-		
-		try {
-			bdr.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return hi;
 	}
 	
 }
